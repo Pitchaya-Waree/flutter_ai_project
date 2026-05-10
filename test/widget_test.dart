5271864bf0cf210f68a5d72b7d50a6f398ef1cf9
Pitchaya Wareerattanaroj <runninghaha123@gmail.com>
2026-05-10 23:36:48 +0700

    Refactor AppBar widget in widget_test.dart for improved readability

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -28,9 +28,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter AI Test App'),
-        ),
+        appBar: AppBar(title: const Text('Flutter AI Test App')),
         body: Center(
           child: Text(
             '$_counter',
